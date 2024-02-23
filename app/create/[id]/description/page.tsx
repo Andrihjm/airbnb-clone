c9a50fbf636049cf091f0135a3f19a180eafe21f
andrihjm <andriisumsel@gmail.com>
2024-02-23 13:54:58 +0700

    create description

--- app/create/[id]/description/page.tsx
+++ app/create/[id]/description/page.tsx
@@ -1,7 +1,67 @@
+import { Card, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
 const page = () => {
   return (
-    <div>kita harus mayir dalam bahasa inggris</div>
-  )
-}
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tighter transition-colors">
+          Pleace description your home as good as you can!
+        </h2>
+      </div>
 
-export default page
+      <form>
+        <div className="w-3/5 mx-auto flex flex-col mt-10 gap-y-5 mb-36">
+          <div className="flex flex-col gap-y-2">
+            <Label>Title</Label>
+            <Input
+              type="text"
+              name="title"
+              required
+              placeholder="Short and Simple...."
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Description</Label>
+            <Textarea
+              name="description"
+              required
+              placeholder="Please describe your home...."
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Price</Label>
+            <Input
+              type="number"
+              name="price"
+              required
+              placeholder="Price per Night in USD"
+              min={10}
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <Label>Image</Label>
+            <Input type="file" name="image" required />
+          </div>
+
+          <Card>
+            <CardHeader className="flex flex-col gap-y-5">
+              <div className="flex items-center justify-between">
+                <div className="flex flex-col">
+                  <h3 className="underline font-medium"></h3>
+                  <p className="text-muted-foreground text-sm">
+                    How many guests do you want?
+                  </p>
+                </div>
+              </div>
+            </CardHeader>
+          </Card>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default page;
