f21cb87f2efddb0708761f51395f89e67af2c2ba
andrihjm <andriisumsel@gmail.com>
2024-02-24 10:24:48 +0700

    add options fitur

--- app/create/[id]/address/page.tsx
+++ app/create/[id]/address/page.tsx
@@ -1,6 +1,33 @@
+"use client"
+
+import { Select, SelectContent, SelectGroup, SelectLabel, SelectTrigger } from "@/components/ui/select"
+import { SelectValue } from "@radix-ui/react-select"
+
 const page = () => {
   return (
-    <div>page address</div>
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="mb-10 text-3xl font-semibold tracking-tighter transition-colors">where is your home located?</h2>
+      </div>
+
+     <form action="">
+        <div className="w-3/5 mx-auto">
+          <div className="mb-5">
+            <Select>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select an Country" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Countries</SelectLabel>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </form> 
+    </>
   )
 }
 
