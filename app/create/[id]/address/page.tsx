4a564841d89dfc8407fcd864efbee92739640723
andrihjm <andriisumsel@gmail.com>
2024-02-24 10:51:26 +0700

    membuat option region

--- app/create/[id]/address/page.tsx
+++ app/create/[id]/address/page.tsx
@@ -1,16 +1,28 @@
-"use client"
+"use client";
 
-import { Select, SelectContent, SelectGroup, SelectLabel, SelectTrigger } from "@/components/ui/select"
-import { SelectValue } from "@radix-ui/react-select"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+} from "@/components/ui/select";
+import { useCountries } from "@/lib/getCounteries";
+import { SelectValue } from "@radix-ui/react-select";
 
 const page = () => {
+  const { getAllCountries } = useCountries();
+
   return (
     <>
       <div className="w-3/5 mx-auto">
-        <h2 className="mb-10 text-3xl font-semibold tracking-tighter transition-colors">where is your home located?</h2>
+        <h2 className="mb-10 text-3xl font-semibold tracking-tighter transition-colors">
+          where is your home located?
+        </h2>
       </div>
 
-     <form action="">
+      <form action="">
         <div className="w-3/5 mx-auto">
           <div className="mb-5">
             <Select>
@@ -21,14 +33,19 @@ const page = () => {
               <SelectContent>
                 <SelectGroup>
                   <SelectLabel>Countries</SelectLabel>
+                  {getAllCountries().map((item) => (
+                    <SelectItem key={item.value} value={item.value}>
+                      {item.flag} {item.label} / {item.region}
+                    </SelectItem>
+                  ))}
                 </SelectGroup>
               </SelectContent>
             </Select>
           </div>
         </div>
-      </form> 
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
