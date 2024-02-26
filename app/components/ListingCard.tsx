e5675ca3bad5aa0e2556d2922fca4ba956e98804
andrihjm <andriisumsel@gmail.com>
2024-02-26 08:23:32 +0700

    fix fitur add product

---
+++ app/components/ListingCard.tsx
@@ -0,0 +1,45 @@
+import { useCountries } from "@/lib/getCounteries";
+import Image from "next/image";
+import Link from "next/link";
+
+interface iAppProps {
+  imagePath: string;
+  description: string;
+  location: string;
+  price: number;
+}
+
+const ListingCard = ({
+  imagePath,
+  description,
+  location,
+  price,
+}: iAppProps) => {
+  const { getCountryByValue } = useCountries();
+  const country = getCountryByValue(location);
+
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-72">
+        <Image
+          src={`https://zlztspkvcjxoshfwcfeg.supabase.co/storage/v1/object/public/images/${imagePath}`}
+          alt="Image of House"
+          fill
+          className="h-full mb-3 rounded-lg object-cover"
+        />
+      </div>
+
+      <Link href={"/"} className="mt-2">
+        <h3 className="font-medium text-base">
+          {country?.flag} {country?.label} {country?.region}
+        </h3>
+        <p className="text-muted-foreground text-sm line-clamp-2">{description}</p>
+        <p className="pt-2 text-muted-foreground">
+          <span className="font-medium text-black">${price}</span> Night
+        </p>
+      </Link>
+    </div>
+  );
+};
+
+export default ListingCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,45 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
-import MapFilterItems from './components/MapFilterItems'
+import ListingCard from "./components/ListingCard";
+import MapFilterItems from "./components/MapFilterItems";
+import prisma from "@/lib/db";
 
-const page = () => {
-  return (
-   <>
-    <div className='container mx-auto px-5 lg:px-10'>
-      <MapFilterItems />
-    </div>
-   </>
-  )   
+async function getData() {
+  const data = await prisma.home.findMany({
+    where: {
+      addedCategory: true,
+      addedLocation: true,
+      addedDescription: true,
+    },
+    select: {
+      id: true,
+      photo: true,
+      price: true,
+      descrption: true,
+      country: true,
+    },
+  });
+  return data;
 }
 
-export default page
+export default async function Home() {
+  const data = await getData();
+
+  return (
+    <>
+      <div className="container mx-auto px-5 lg:px-10">
+        <MapFilterItems />
+
+        <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8">
+          {data.map((item) => (
+            <ListingCard
+              key={item.id}
+              imagePath={item.photo as string}
+              description={item.descrption as string}
+              location={item.country as string}
+              price={item.price as number}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
