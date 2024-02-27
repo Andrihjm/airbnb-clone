8957c8ac2057b2dcf1a137b2b145589fa2673526
andrihjm <andriisumsel@gmail.com>
2024-02-27 07:42:31 +0700

    create my-homes components

--- app/my-homes/loading.tsx
+++ app/my-homes/loading.tsx
@@ -1,11 +1,9 @@
 import SkeletonCard from "../components/SkeletonCard";
 
-const loadingFavotire = () => {
+const loadingMyHomes = () => {
   return (
     <section className="container mx-auto px-5 mt-10 lg:px-10">
-      <h2 className="text-3xl font-semibold tracking-tighter">
-        Yoyur favorite
-      </h2>
+      <h2 className="text-3xl font-semibold tracking-tighter">Yoyur Homes</h2>
 
       <div className="grid lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1 gap-8 mt-8">
         <SkeletonCard />
@@ -21,4 +19,4 @@ const loadingFavotire = () => {
   );
 };
 
-export default loadingFavotire;
+export default loadingMyHomes;
